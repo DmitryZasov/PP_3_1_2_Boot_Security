ff70d9398501c32e2dddb1770ae77935f2a0efc1
DmitryZasov <nomocrew@mail.ru>
2024-08-01 18:40:53 +0500

    updt

--- src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
@@ -1,12 +1,15 @@
 package ru.kata.spring.boot_security.demo.models;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Table(name = "roles")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -72,4 +75,11 @@ public class Role {
     public String toString() {
         return roleName;
     }
+
+
+    @Override
+    public String getAuthority() {
+        return "";
+    }
 }
+
